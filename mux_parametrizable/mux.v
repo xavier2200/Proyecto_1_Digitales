cc5bdc916579ada7b31caab7f32aeb99595a9f10
Xavier Barrios <xbs20@hotmail.com>
2024-03-12 10:19:28 -0600

    mux parametrizable 2 in

--- mux_parametrizable/mux.v
+++ mux_parametrizable/mux.v
@@ -1,21 +1,15 @@
-module mux #(parameter maxnum=3, parameter depth=4)(
-    input clk,
-    input rst,
-    input [$clog2(depth)-1:0] sel,
-    input [depth-1:0][$clog2(maxnum)-1:0] inputs,
-    output [$clog2(maxnum)-1:0] salida
+module mux #(ancho=3)(
+input sel,
+input [ancho-1:0] in_1,
+input [ancho-1:0] in_2,
+output [ancho-1:0] salida
 );
 
-wire [depth-1:0]en;
-genvar i;
-generate
-    for (i = 0;i < depth; i=i+1 ) begin
-        assign en[i]=(i==sel)? 1:0;
-    end
-    for(i = 0; i<depth; i=i+1) begin
-        bufferz #(maxnum)bufferz_(.en(en[i]), .entrada(salida_ff[i]));
-    end
-endgenerate
-
-
+always @* begin
+    case (sel)
+        1'b0 : salida=in_1;
+        1'b1 : salida=in_2;
+        default: salida=in_1;
+    endcase
+end
 endmodule
