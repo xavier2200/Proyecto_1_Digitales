4f8ccd88ca694b7abed056620973cc43d3e858db
Lesmes Torres <lesmestorres@estudiantec.cr>
2024-03-30 17:17:22 -0600

    pc_autoverificalbe

--- pc_counter/pc_tb.sv
+++ pc_counter/pc_tb.sv
@@ -4,48 +4,50 @@ module pc_tb;
 
     parameter ADDRESS_WIDTH = 32;
     // Señales de entrada
-    logic rst;
-    logic clk;
-    logic [ADDRESS_WIDTH-1:0] PC_next;
+    logic rst_tb;
+    logic clk_tb;
+    logic [ADDRESS_WIDTH-1:0] PC_next_tb;
 
     // Señales de salida
-    logic [ADDRESS_WIDTH-1:0] address_o;
+    logic [ADDRESS_WIDTH-1:0] address_o_tb;
     
 
     // Instancia del contador de programa
-    pc_counter #(
-        .ADDRESS_WIDTH(ADDRESS_WIDTH)
-    ) dut (
-        .rst(rst),
-        .clk(clk),
-        .PC_next(PC_next),
-        .address_o(address_o)
+    pc_counter #(.ADDRESS_WIDTH(ADDRESS_WIDTH)) PC_dut (
+        .rst(rst_tb),
+        .clk(clk_tb),
+        .PC_next(PC_next_tb),
+        .address_o(address_o_tb)
     );
 
     // Señal de reloj
-    always #5 clk = ~clk;
+    always #5 clk_tb = ~clk_tb;
     
-
     // Estímulo
     initial begin
-        PC_next = 0;
-        clk =1;
-        // Reset inicial
-        rst = 1;
-        #10 rst = 0;
+        // Inicialización de señales
+        PC_next_tb = 0;
+        clk_tb = 0;
+        
+        // Generar un pulso de reset
+        rst_tb = 1;
+        #10 rst_tb = 0;
 
-        // Señales de entrada
-        #20 PC_next = 1;
-        #20 PC_next = 2;
-        // Continuar con más cambios de PC_next según sea necesario
-
-        // Finalizar simulación después de un tiempo
-        #1000 $finish;
+        // Generar estímulo utilizando un bucle for
+        for (int i = 0; i < 6; i = i + 1) begin 
+            // Establecer la próxima dirección del programa
+            PC_next_tb = i;
+            // Esperar un ciclo de reloj
+            #5;
+            
+            if(PC_next_tb != i)begin 
+                $display("Error, contenido incorrecto");       
+            end else 
+                $display("address_o = %h", address_o_tb);
+        end
+        $finish;    
     end
-
-    // Visualización de resultados
-    always @(posedge clk) begin
-        $display("address_o = %h", address_o);
-    end
-
 endmodule
+
+
+
