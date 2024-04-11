d9bb12304d28ae2fa1de69d170cc0fd42164083e
xavier2200 <xbs20@hotmail.com>
2024-04-10 19:19:11 -0600

    correccion lectura de mememoria

--- design/inst_memory.sv
+++ design/inst_memory.sv
@@ -1,7 +1,7 @@
 module inst_memory #(
     parameter DATA_WIDTH=32,
     parameter ADDRESS_WIDTH=32, // EN REALIDAD NO NECESITAMOS TODOS LOS BITS POR EL TAMAÑO DE LA MEMORY
-    parameter MEM_SIZE=256
+    parameter MEM_SIZE=10
 ) (
     input logic [ADDRESS_WIDTH-1:0] address,
     output logic [DATA_WIDTH-1:0] instruction
@@ -13,9 +13,9 @@ module inst_memory #(
 
 
     initial begin
-        $readmemh("programa.bin",inst_memory);
+        $readmemh("programa.hex",inst_memory,0,9);
     end
 
-    assign instruction=inst_memory[address[9:2]];
+    assign instruction=inst_memory[address[9:2]]; //hacer en data memory
     
 endmodule
